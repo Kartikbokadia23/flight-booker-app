c9d63e56fb4fac0b3e10ef24119824798a29d006
kartikbokadia23 <kartikbokadia@gmail.com>
2021-02-11 17:43:53 +0530

    associated flights bookings and passengers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :flights
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'flights#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :flights, only: [:index]
+  resources :bookings, only: [:new, :create, :show]
 end
